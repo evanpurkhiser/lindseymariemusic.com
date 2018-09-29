980fbb0481ccaa010d5d813dc21fd0e3d2edc6a5
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-09-28 22:39:10 -0500

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, '../dist/assets'),
+    filename: 'app.js',
+  },
+  devtool: 'source-map',
+  devServer: { port: 9000 },
+  module: {
+    loaders: [{
+      test:   /\.js$/,
+      loader: 'babel-loader',
+      query: { presets: ['env', 'stage-1', 'react'] },
+    },
+    {
+      test:   /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+      loader: 'file-loader',
+    }],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html' })],
+};
